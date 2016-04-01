39365209770c29fb95c5e464e6990993c7dce53e
Inooid <justdrawcats@gmail.com>
2016-04-01 12:41:30 +0200

    Compress images

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
 
-
-gulp.task('copy-html', function() {
+gulp.task('html', function() {
   return gulp.src('src/*.html')
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('minify-css', function() {
+gulp.task('style', function() {
   return gulp.src('src/style.css')
     .pipe(cleanCSS({ compatibility: 'ie11' }))
     .pipe(gulp.dest('dist'));
 });
 
+gulp.task('images', function() {
+  return gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
+});
+
 gulp.task('default', function() {
-  gulp.start('copy-html');
-  gulp.start('minify-css');
+  gulp.start('html');
+  gulp.start('style');
+  gulp.start('images');
 });
